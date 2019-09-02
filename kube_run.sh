afb101bc2f4d36c1ca197a56cb9db07e08942654
Ubuntu <wendy.a.mayorga@gmail.com>
2019-09-02 23:58:08 +0000

    name matches

--- kube_run.sh
+++ kube_run.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=blue1
+dockerpath=bluegreen1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run $dockerpath --image=gwenevere05/blue1
+kubectl run $dockerpath --image=gwenevere05/bluegreen1
 
 
 # Step 3:
@@ -19,5 +19,5 @@ kubectl get pods
 # Forward the container port to a host
 #https://github.com/dennyzhang/cheatsheet-kubernetes-A4
 #kubectl expose deployment my-app --type=LoadBalancer --name=my-service
-kubectl expose deployment blue1 --type=LoadBalancer --port=80
-minikube service blue
+kubectl expose deployment bluegreen1 --type=LoadBalancer --port=80
+minikube service bluegreen1
